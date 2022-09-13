69751c30112192b0d7e2eb4a5659a331c0b4ecca
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-13 11:16:30 -0500

    First commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine("--------------------------------------");
+Console.WriteLine($"Hello, how are you doing {name}?");
+var condition = Console.ReadLine();
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Ok...... how old are you?");
+int age = Convert.ToInt32(Console.ReadLine());
+if (age <= 125)
+    Console.WriteLine("That is a good age to be");
+else
+    Console.WriteLine("Woah....... your different!");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Last question!");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("What is your favorite type of tree? ONLY ONE!");
+var tree = Console.ReadLine();
+Console.WriteLine("--------------------------------------");
+Console.WriteLine($"Ooooo..... thats a nice one! Wait! {name}! One more question!");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine($"What is your favorite type of video game console {age} year old {name}? ONLY ONE!");
+var videoGameConsole = Console.ReadLine();
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Wow..... your good at this.... I guess I can let you go now :)");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine($"Press enter to continue");
+var info = Console.ReadLine();
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("Thanks for letting me suck up your information! :) Bye!-----------------------------------------------------------");
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+
+
+
+
+
+
